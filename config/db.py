de96a7179e9382e2d6b7e64d6d229e35fc8aa5eb
Romualdo <romualdoperez014@gmail.com>
2024-07-19 14:29:48 -0600

    bd

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_zFbNPmusoKYQlJny6g9@hospital-romualdoperez014-d42d.h.aivencloud.com:11045/defaultdb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
